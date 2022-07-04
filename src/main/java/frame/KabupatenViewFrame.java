856c0995950974bfaa03ae56c63cf6f65fd89d1d
rahmadhani08 <rahmadaniapp892@gmail.com>
2022-07-04 22:03:34 +0800

    Penambahan Validasi Data Kosong dan Data Sama

--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -52,6 +52,14 @@ public class KabupatenViewFrame extends JFrame{
             isiTable();
         });
         cariButton.addActionListener(e -> {
+            if(cariTextField.getText().equals("")){
+                JOptionPane.showMessageDialog(null,"" +
+                        "Isi kata kunci pencarian",
+                        "Validasi kata kunci kosong",
+                        JOptionPane.WARNING_MESSAGE);
+                cariTextField.requestFocus();
+                return;
+            }
             Connection c = Koneksi.getConnection();
             String keyword = "%" + cariTextField.getText() + "%";
             String searchSQL = "SELECT * FROM kabupaten WHERE nama like ?";
